d82289b7892fcef33d3a5e7da9485c1025a3e554
陳思瑜 Judy <laplus.space@gmail.com>
2025-09-28 11:54:21 +0800

    chore(all): init

---
+++ rspress.config.ts
@@ -0,0 +1,50 @@
+import * as path from 'node:path';
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+  base: '/worklog',
+  root: path.join(__dirname, 'docs'),
+  outDir: path.join(__dirname, 'dist'),
+  globalStyles: path.join(__dirname, 'styles/index.css'),
+  title: 'Worklog',
+  icon: 'https://avatars.githubusercontent.com/u/105040809',
+  logo: 'https://avatars.githubusercontent.com/u/105040809',
+  themeConfig: {
+    lastUpdated: true,
+    enableScrollToTop: true,
+    socialLinks: [
+      {
+        icon: 'github',
+        mode: 'link',
+        content: 'https://github.com/laplus-x/laplus-x',
+      },
+    ],
+  },
+  multiVersion: {
+    default: 'latest',
+    versions: ['latest'],
+  },
+  search: {
+    versioned: true,
+  },
+  ssg: false,
+  builderPlugins: [],
+  builderConfig: {
+    resolve: {
+      alias: {
+        '@': './src',
+      },
+    },
+    dev: {
+      hmr: true,
+      liveReload: true,
+      watchFiles: {
+        paths: ['docs/**/*.md'],
+        type: 'reload-page',
+      },
+    },
+    output: {
+      assetPrefix: '/worklog'
+    }
+  }
+});
