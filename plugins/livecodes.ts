388dda82f4ed9c7ea188b5ec83991e2379688909
陳思瑜 Judy <laplus.space@gmail.com>
2025-10-18 20:33:56 +0800

    feat(plugins): 新增 livecodes plugin

--- plugins/livecodes.ts
+++ plugins/livecodes.ts
@@ -1,9 +1,18 @@
 
+import type { EmbedOptions } from "livecodes";
 import remarkLivecodes from "remark-livecodes";
+import type { RspressPlugin } from "rspress/core";
 
-export const livecodes = () => ({
+type Options = Omit<EmbedOptions, "headless"> & {
+    auto?: boolean
+    render?: "button" | "link" | "playground" | "meta"
+    height?: string
+    className?: string
+};
+
+export const livecodes = (options?: Options): RspressPlugin => ({
     name: 'rspress-plugin-livecodes',
     markdown: {
-        remarkPlugins: [remarkLivecodes],
+        remarkPlugins: [() => remarkLivecodes(options)],
     },
 });
--- rspress.config.ts
+++ rspress.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
         }
       }
     }),
-    livecodes()
+    livecodes({ render: "button" }),
   ],
   builderConfig: {
     resolve: {
